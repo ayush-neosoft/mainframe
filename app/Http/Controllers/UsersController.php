8d0230d17d74e1000557424cf4fc0d214109400a
Ayush Likhar <ayush.likhar@neosofttech.com>
2020-02-24 15:01:57 +0530

    index page users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -13,7 +14,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        return 'test';
+        $users = User::all();
+        return view('users.index', ['users' => $users]);
     }
 
     /**
